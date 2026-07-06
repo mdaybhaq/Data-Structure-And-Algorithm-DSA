4022c7d93d8ddce0d4a5740644553978e21a4df2
mdaybhaq <mdaybhaq@gmail.com>
2026-07-06 20:26:15 +0530

    DSA

---
+++ treeRecursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void treeRecursion(int n){
+	if(n>0){
+
+		printf("%d \t", n);
+
+		treeRecursion(n-1);
+		treeRecursion(n-1);
+	}
+}
+
+int main(){
+
+	treeRecursion(3);
+	return 0;
+}
